79253f5b2752521b2e298cdc77c9328bfea0dc61
Benedikt Rips <benedikt.rips@gmail.com>
2024-08-22 14:15:44 +0200

    build: specify dependencies via Nix

---
+++ dependencies.nix
@@ -0,0 +1,52 @@
+{
+  pkgs = pkgs: with pkgs; [
+    python3Packages.pygments
+  ];
+  texlivePkgs = texlivePkgs: with texlivePkgs; [
+    amscls
+    amsfonts
+    beamer
+    biber
+    biblatex
+    booktabs
+    csquotes
+    ellipsis
+    enumitem
+    etoolbox
+    fancyvrb
+    fontspec
+    geometry
+    hyperref
+    hyphen-english
+    hyphen-german
+    inconsolata
+    koma-script
+    lastpage
+    latexmk
+    libertine
+    luacolor
+    luatexbase
+    mathdots
+    mathtools
+    mdframed
+    microtype
+    minted
+    ncctools # extdash
+    needspace
+    newfloat
+    newtx
+    oberdiek # ifdraft, iflang
+    polyglossia
+    relsize
+    selnolig
+    setspace
+    tikz-cd
+    todonotes
+    tools # multicol, varioref
+    txfonts # required by newtx
+    upquote
+    xcolor
+    xurl
+    zref
+  ];
+}
